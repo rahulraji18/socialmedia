15fa1130ad869479361984b2fbe719cbde5d96f6
rahul <rahul.rajeevan@spericorn.com>
2022-10-04 16:02:09 +0530

    user follow and unfollow

--- controllers/user/user.js
+++ controllers/user/user.js
@@ -1,5 +1,6 @@
 const userModel = require('../../db/models/user');
 const catchAsyncError = require('../../middlewares/catchAsyncError');
+const ErrorHandler = require('../../utils/ErrorHandling')
 
 exports.update = catchAsyncError(async (req,res,next) => {
     const {password,...others} = req.body;
@@ -36,4 +37,41 @@ exports.view = catchAsyncError(async (req,res,next) => {
     const user = await userModel.findById(req.params.id);  
     const {password, udatedAt, ...other} = user._doc;
     res.status(200).json(other);
+})
+
+exports.follow = catchAsyncError(async (req,res,next) => {
+    if(req.body.userId !== req.params.id){
+            const user = await userModel.findById(req.params.id);
+            const currentUser = await userModel.findById(req.body.userId)
+            if(!user.followers.includes(req.body.userId)){
+                await user.updateOne({$push: {followers: req.body.userId}})
+                await currentUser.updateOne({$push: {following: req.params.id}})
+                res.status(200).json('user has been followed')
+            }
+            else{
+                return next(new ErrorHandler('You already follow this user',404));
+            }
+        }
+     else{
+         return next(new ErrorHandler('you cant follow yourself',404));
+           }
+
+})
+
+exports.unfollow = catchAsyncError(async (req,res,next) => {
+    if(req.body.userId !== req.params.id){
+            const user = await userModel.findById(req.params.id);
+            const currentUser = await userModel.findById(req.body.userId)
+            if(user.followers.includes(req.body.userId)){
+                await user.updateOne({$pull: {followers: req.body.userId}})
+                await currentUser.updateOne({$pull: {following: req.params.userId}})
+                res.status(200).json('user has been unfollowed')
+            }
+            else{
+                return next(new ErrorHandler('You dont follow this user',404));
+            }
+    }
+    else{
+        res.status(403).json('You cant unfollow yourself');
+    }userModel
 })
--- routes/users/users.js
+++ routes/users/users.js
@@ -7,6 +7,8 @@ router.delete('/:id', userController.delete);
 // Get a user
 router.get('/:id', userController.view);
 // Follow user
+router.put('/:id/follow', userController.follow);
 // Unfollow user
+router.put('/:id/unfollow', userController.unfollow);
 
 module.exports = router;
